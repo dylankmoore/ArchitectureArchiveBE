c26e4e556420837fd06c6aee3abf78e5b7b93249
Dylan Moore <dylankmoore@gmail.com>
2024-05-20 21:48:03 -0500

    seeded data

--- Data/StyleData.cs
+++ Data/StyleData.cs
@@ -5,7 +5,21 @@ namespace ArchitectureArchiveBE.Data
     {
         public static List<Style> Styles = new List<Style>
         {
-
+            new Style { StyleId = 1, Name = "Gothic" },
+            new Style { StyleId = 2, Name = "Baroque" },
+            new Style { StyleId = 3, Name = "Neoclassical" },
+            new Style { StyleId = 4, Name = "Art Deco" },
+            new Style { StyleId = 5, Name = "Modern" },
+            new Style { StyleId = 6, Name = "Postmodern" },
+            new Style { StyleId = 7, Name = "Renaissance" },
+            new Style { StyleId = 8, Name = "Victorian" },
+            new Style { StyleId = 9, Name = "Romanesque" },
+            new Style { StyleId = 10, Name = "Colonial" },
+            new Style { StyleId = 11, Name = "Ancient Chinese" },
+            new Style { StyleId = 12, Name = "Mughal/Islamic" },
+            new Style { StyleId = 13, Name = "Inca/Pre-Columbian" },
+            new Style { StyleId = 14, Name = "Gothic Revival" },
+            new Style { StyleId = 15, Name = "Classical" }
         };
     }
 }
--- Data/TagData.cs
+++ Data/TagData.cs
@@ -5,7 +5,17 @@ namespace ArchitectureArchiveBE.Data
     {
         public static List<Tag> Tags = new List<Tag>
         {
-
+            new Tag { TagId = 1, Name = "National Monument" },
+            new Tag { TagId = 2, Name = "Tourist Attraction" },
+            new Tag { TagId = 3, Name = "Endangered" },
+            new Tag { TagId = 4, Name = "Art Installation" },
+            new Tag { TagId = 5, Name = "Renovated" },
+            new Tag { TagId = 6, Name = "Cultural Landmark" },
+            new Tag { TagId = 7, Name = "Museum" },
+            new Tag { TagId = 8, Name = "Under Construction" },
+            new Tag { TagId = 9, Name = "Closed for Renovation" },
+            new Tag { TagId = 10, Name = "Sustainable" },
+            new Tag { TagId = 11, Name = "Innovation"}
         };
     }
 }
